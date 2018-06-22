ef2423ad21141abed3a32d9ab74a85e4e7653845
Brandon Divis <bdivis@gmail.com>
2018-06-22 17:56:35 -0500

    Update hamburger; rearrange elements; add .vs/ to .gitignore

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 var week = [
-  'The weekend is over :(',
-  'It\'s Monday? Gross!',
-  'Taco Tuesday boiiii!',
-  'It\'s Wednesday, my dudes. AAAAAAHHHHH!',
-  'It\'s thirsty Thursday!',
-  'Enjoy your weekend!',
-  'It\'s Saturday! Turn up!'];
+  "The weekend is over :(",
+  "It's Monday? Gross!",
+  "Taco Tuesday boiiii!",
+  "It's Wednesday, my dudes. AAAAAAHHHHH!",
+  "It's thirsty Thursday!",
+  "Enjoy your weekend!",
+  "It's Saturday! Turn up!"
+];
 
 var today = new Date();
 var day = today.getDay();
-var greeting = '';
+var greeting = "";
 if (today.getHours() < 12) {
-  greeting = 'Good morning!';
+  greeting = "Good morning!";
 } else if (today.getHours() < 16) {
-  greeting = 'Good afternoon!';
+  greeting = "Good afternoon!";
 } else {
-  greeting = 'Good evening!';
+  greeting = "Good evening!";
 }
 
-var x = greeting + " " + week[day]);
+var x = greeting + " " + week[day];
+document.getElementById("greeting").innerText = x;
+
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', (event) => {
+  hamburger.classList.toggle('active');
+});
